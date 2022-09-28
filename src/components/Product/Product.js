789b56ed5d541e536b3e1cc2c1d2786e63a0dade
bidduthosen <mdbidduthosen34@gmail.com>
2022-09-29 01:27:58 +0600

    dynamic cart name age and button added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Product.css'
 
 const Product = (props) => {
-    const {name, picture, Time, age, about} =props.product
+    const {name, picture, time, age, about} =props.product
     console.log(props.product)
     return (
         <div className='products'>
@@ -10,10 +10,12 @@ const Product = (props) => {
                 <div class="card">
                     <img src={picture} class="card-img-top" alt="..."/>
                     <div class="card-body">
-                    <h5 class="card-title">{name}</h5>
+                    <h5 class="card-title">Name: {name}</h5>
                     <p>{about}</p>
-                    
+                    <h6>For Age: {age}</h6>
+                    <h6>Time required: {time}s</h6>
                     </div>
+                <button className='border-0 p-3'>Add to list</button>
                 </div>
             </div>
         </div>
