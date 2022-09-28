ac8a64a6238d8e3ab04e08c50d6533c336b856c1
bidduthosen <mdbidduthosen34@gmail.com>
2022-09-29 04:03:06 +0600

    my info submit and edit

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -10,6 +10,21 @@ const Cart = () => {
                 <h4>Md:Biddut Hosen</h4>
                 <h6><FontAwesomeIcon icon={faLocationDot}></FontAwesomeIcon> <small>mymensingh, dhaka</small></h6>
             </div>
+            <div className='my-info d-md-flex justify-content-md-around'> 
+                <div>
+                    <h6>59<sub>kg</sub></h6>
+                    <h6><sub>Width</sub></h6>
+                </div>  
+                <div>
+                    <h6>5.11</h6>
+                    <h6><sub>Higth</sub></h6>
+                </div>
+                <div>
+                    <h6>22<sub>yrs</sub></h6>
+                    <h6><sub>Age</sub></h6>
+                </div>
+            </div>
+
         </div>
     );
 };
